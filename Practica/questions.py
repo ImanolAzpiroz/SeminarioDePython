80ca493824a7ed5601798b0efc761e032657e033
Imanol Azpiroz <imanolazpiroz77@gmail.com>
2026-03-16 07:27:45 -0300

    Update questions.py
    Se agregaron categorías en la elección de lista de palabras

--- Practica/questions.py
+++ Practica/questions.py
@@ -1,17 +1,14 @@
 import random
+import sys
 
-words = [
-    "python",
-    "programa",
-    "variable",
-    "funcion",
-    "bucle",
-    "cadena",
-    "entero",
-    "lista",
-]
+words_dic = {
+    "Fulbo" : ["independiente", "river", "boca", "racing", "huracan"],
+    "Series" : ["breaking", "thrones", "jujutsu", "shingeki", "mandalorian"],
+    "Top5 mejores guitarristas" : ["gardiner", "wong", "henson", "danel", "zytecki", "rafa", "poniaseisno"],
+    "Placebos" : ["nada", "vacio", "cero", "nulo"],
+    "Standard" : ["python", "programa", "variable", "funcion", "bucle", "cadena", "entero", "lista"]
+}
 
-word = random.choice(words)
 guessed = []
 attempts = 6
 
@@ -19,6 +16,20 @@ points = 0
 
 print("¡Bienvenido al Ahorcado!")
 print()
+
+# Interfaz para seleccionar categoría (Usa enumerate para poder usar un indice en la seleccion)
+print("Seleccione una categoria: ")
+for i, category in enumerate(words_dic.keys(), 1):
+    print(f"{i}. {category}")
+
+category_choice = int(input())
+
+# Seleccionar una palabra aleatoria de la categoría elegida:
+# Se debe convertir en lista para acceder a las claves por indice
+# List(words_dic.keys()) devuelve una lista de claves y [category_choice - 1] es el indice
+# Luego con la clave accede al diccionario y hace la seleccion random
+# print(words_dic[list(words_dic.keys())[category_choice - 1]])           # (Esto mostraria una de las listas)
+word = random.choice(words_dic[list(words_dic.keys())[category_choice - 1]])
 
 while attempts > 0:
 # Mostrar progreso: letras adivinadas y guiones para las que faltan
@@ -45,6 +56,7 @@ while attempts > 0:
     # Validar que el input sea una sola letra y no un número o símbolo
     if len(user_input) != 1 or not user_input.isalpha():
         print("Entrada no valida")
+        print()
         continue
 
     if user_input in guessed:
