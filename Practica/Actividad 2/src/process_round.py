1e52ae0a027fae059e97798e55aeb9ae55465a5b
Imanol Azpiroz <imanolazpiroz77@gmail.com>
2026-04-05 15:11:53 -0300

    TP2 - E10 - Init
    Creacion de carpetas, inicializacion de constantes, creacion del venv, estructura del proyecto, estructura de metodos

---
+++ Practica/Actividad 2/src/process_round.py
@@ -0,0 +1,30 @@
+
+def update_stats(stats, round):
+    """
+    Función que actualiza las estadísticas de los participantes después de cada ronda.
+    
+    Args:
+        stats (dict): Un diccionario con los nombres de los participantes como claves y otro diccionario con sus estadísticas como valores.
+        round (dict): Un diccionario con el tema de la ronda y las puntuaciones de los participantes.
+        
+    Returns:
+        dict: El diccionario actualizado con las nuevas estadísticas.
+    """
+
+
+
+
+def process(round, stats):
+    """
+    Función que procesa la información de una ronda y actualiza las estadísticas de los participantes.
+    
+    Args:
+        round (dict): Un diccionario con el tema de la ronda(str) y las puntuaciones de los participantes(dict).
+        stats (dict): Un diccionario con los nombres de los participantes como claves y otro diccionario con sus estadísticas como valores.
+        
+    Returns:
+        None
+    """
+
+
+    
